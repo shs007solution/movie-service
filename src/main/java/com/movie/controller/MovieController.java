fa67f02dec84dbd9a2e49bb3d67be438db412b2a
Suchait Gaurav <>
2022-10-23 19:44:01 +0530

    changing controller response.

--- src/main/java/com/movie/controller/MovieController.java
+++ src/main/java/com/movie/controller/MovieController.java
@@ -18,7 +18,8 @@ public class MovieController {
         Movie m1 = new Movie(12L, "movie-name-1");
         Movie m2 = new Movie(13L, "movie-name-2");
         Movie m3 = new Movie(14L, "movie-name-3");
+        Movie m4 = new Movie(14L, "movie-name-4");
 
-        return List.of(m1, m2, m3);
+        return List.of(m1, m2, m3, m4);
     }
 }
