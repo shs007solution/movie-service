32828f9abca5af65109b2071c45a880e5d913b80
Suchait Gaurav <>
2022-12-21 21:25:33 +0530

    Adding more value in response.

--- src/main/java/com/movie/controller/MovieController.java
+++ src/main/java/com/movie/controller/MovieController.java
@@ -2,6 +2,8 @@ package com.movie.controller;
 
 
 import com.movie.dtos.Movie;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +12,11 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/pick")
+@RequiredArgsConstructor
 public class MovieController {
+
+    @Value("${custom.property.app.one}")
+    private String customAppValueOne;
 
     @GetMapping("/movies")
     public List<Movie> getMovies() {
@@ -19,7 +25,14 @@ public class MovieController {
         Movie m2 = new Movie(13L, "movie-name-2");
         Movie m3 = new Movie(14L, "movie-name-3");
         Movie m4 = new Movie(14L, "movie-name-4");
+        Movie m5 = new Movie(14L, "movie-name-45");
 
-        return List.of(m1, m2, m3, m4);
+        return List.of(m1, m2, m3, m4, m5);
     }
+
+    @GetMapping("/app/value")
+    public List<String> getAppValue() {
+        return List.of(customAppValueOne);
+    }
+
 }
