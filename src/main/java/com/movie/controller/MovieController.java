49b173f1461e66a34a48c8482af34cf52a60fbb7
Suchait Gaurav <>
2022-12-30 18:29:09 +0530

    reverting change.

--- src/main/java/com/movie/controller/MovieController.java
+++ src/main/java/com/movie/controller/MovieController.java
@@ -25,9 +25,8 @@ public class MovieController {
         Movie m2 = new Movie(13L, "movie-name-2");
         Movie m3 = new Movie(14L, "movie-name-3");
         Movie m4 = new Movie(14L, "movie-name-4");
-        Movie m5 = new Movie(14L, "movie-name-45");
 
-        return List.of(m1, m2, m3, m4, m5);
+        return List.of(m1, m2, m3, m4);
     }
 
     @GetMapping("/app/value")
